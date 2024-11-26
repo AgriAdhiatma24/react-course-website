bea3b4d2c2cf0b0d80f663e156b6fd1fd6fe7a9b
unknown <agriadhiatma@gmail.com>
2024-11-27 00:27:52 +0700

    fix: ScrollToTop path

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./main.css";
-import ScrollToTop from "./components/scrollToTop.jsx";
 import "animate.css";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
+import ScrollToTop from "./components/ScrollToTop.jsx";
 import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
